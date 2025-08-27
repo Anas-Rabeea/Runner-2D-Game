4cbae15606c036715e3f550a9eafa126a3752d24
Anas-Rabeea <AnasRabeea001@gmail.com>
2025-08-27 12:15:37 +0300

    For loop

--- Test.java
+++ Test.java
@@ -5,6 +5,10 @@ public class test
     public static void main (String[] args)
     {
             System.out.println("New Line");
+            for (int i = 0 ; i < 10 ; i++)
+            {
+                System.out.println("Value is " + i );
+            }
     }
 
 
